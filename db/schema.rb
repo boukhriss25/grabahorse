d27d2464074ce1a1671dbf45cf4147505109c2c1
Mehdi Idrissi <idrissikaitounimehdi@gmail.com>
2019-08-12 17:56:13 +0200

    last scjema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_150949) do
+ActiveRecord::Schema.define(version: 2019_08_12_154005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.string "status"
+    t.bigint "user_id"
+    t.bigint "horse_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["horse_id"], name: "index_bookings_on_horse_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "horses", force: :cascade do |t|
     t.string "name"
@@ -39,5 +52,7 @@ ActiveRecord::Schema.define(version: 2019_08_12_150949) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "horses"
+  add_foreign_key "bookings", "users"
   add_foreign_key "horses", "users"
 end
