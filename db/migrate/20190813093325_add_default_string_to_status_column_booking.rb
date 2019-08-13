2a67523b8bab974d6338536e43aefc4f2e93f2e9
Glen Warren <glenwarren@zoho.com>
2019-08-13 12:03:47 +0200

    validations added to horse and booking models, and migration added to add default value to status column of bookings, and horses routes added

--- db/migrate/20190813093325_add_default_string_to_status_column_booking.rb
+++ db/migrate/20190813093325_add_default_string_to_status_column_booking.rb
@@ -1,5 +1,6 @@
 class AddDefaultStringToStatusColumnBooking < ActiveRecord::Migration[5.2]
   def change
-    change_column :bookings, :status, default: "pending"
+    remove_column :bookings, :status
+    add_column :bookings, :status, :string, default: "pending"
   end
 end
