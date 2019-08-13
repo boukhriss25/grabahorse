2f46c31f4afafff66d89aa9203a2acdde22e6f3b
Glen Warren <glenwarren@zoho.com>
2019-08-13 13:34:33 +0200

    halfway through adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database of all users...'
+User.destroy_all
+puts 'Initiating users seed...'
+
+15.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: "password"
+    )
+end
+
+puts 'Cleaning database of all horses...'
+Horse.destroy_all
+puts 'Initiating horses seed...'
+
+20.times do
+  Horse.create(
+    name: Faker::Creature::Horse.name,
+    location: Faker::Address.city,
+    price: rand(25..75),
+    description: Faker::Coffee.notes,
+    user_id: rand(1..15)
+    )
+end
+
+
