bee45d729bf5779892466a9c6d6c8befbe87d764
Glen Warren <glenwarren@zoho.com>
2019-08-16 14:47:58 +0200

    enabled search without being logged in, and redirect user back to horse after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || search_path
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
