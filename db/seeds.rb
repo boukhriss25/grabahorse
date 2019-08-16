784b2d9f5a5839cd0eea56da5c6044520c637e69
Amine Boukhriss <boukhrissamine@gmail.com>
2019-08-16 12:34:06 +0200

    added new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,26 @@ User.create(
   password: "password"
 )
 
-# puts 'Cleaning database of all horses...'
-# Horse.destroy_all
-# puts 'Initiating horses seed...'
+puts 'Cleaning database of all horses...'
+Horse.destroy_all
+puts 'Initiating horses seed...'
 
-# 20.times do
-#   Horse.create(
-#     name: Faker::Creature::Horse.name,
-#     location: Faker::Address.city,
-#     price: rand(25..75),
-#     description: Faker::Coffee.notes,
-#     user: User.all.sample
-#     )
-# end
+locations = ["Barcelona", "Paris", "Casalanca", "Medellin", "Milan", "Swindon"]
+
+images = [
+  "https://www.sciencenews.org/sites/default/files/2018/02/main/articles/022118_EE_horses_feat.jpg",
+  "https://images2.minutemediacdn.com/image/upload/c_crop,h_1194,w_2121,x_0,y_34/f_auto,q_auto,w_1100/v1553786510/shape/mentalfloss/539787-istock-879570436.jpg"
+]
+
+20.times do
+  Horse.create(
+    name: Faker::Creature::Horse.name,
+    location: locations.sample,
+    price: rand(25..75),
+    description: Faker::Coffee.notes,
+    user: User.all.sample,
+    remote_image_url: images.sample
+    )
+end
 
 
